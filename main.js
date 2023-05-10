0e078b229f75aae406fa8eec6f7b6d9bd377bd17
Bruno Canini <brunocanini1994@gmail.com>
2023-05-10 18:26:34 +0200

    prova

---
+++ main.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        newTask : "",
+        taskList : [
+            { text : "lavare la macchina", done: false },
+            { text :"tagliare l erba", done: false },
+            { text :"montare mobile", done: false }
+        ]
+      }
+    },
+    methods: {
+        addTask(){
+            if( this.newTask.length >= 3){
+                let newTask = {
+                    text : this.newTask,
+                    done : false
+                }
+                this.taskList.push(newTask);
+                this.newTask = ""; 
+            }
+        },
+        removeTask(i){
+            this.taskList.splice(i, 1);
+        },
+        changeTask(i){
+            this.taskList[i].done= !this.taskList[i].done;
+        }
+      },
+    mounted() {
+
+      }
+  }).mount('#app')
